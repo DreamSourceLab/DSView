ac1d16041158535a7e616f44e9223be5c12ec0f9
DreamSourceLab <support@dreamsourcelab.com>
2022-01-27 19:19:50 -0800

    move decoders

--- decoders/st7789/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-This decoder decodes the Sitronix ST7789 TFT controller protocol.
-
-Details: https://www.newhavendisplay.com/appnotes/datasheets/LCDs/ST7789V.pdf
-"""
-
-from .pd import Decoder
