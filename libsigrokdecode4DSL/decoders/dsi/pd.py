e27ab19bf5555dc7828ed072e54ae8d18bc4635f
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-08-08 13:11:07 +0200

    Update DSI decoder

--- libsigrokdecode4DSL/decoders/dsi/pd.py
+++ libsigrokdecode4DSL/decoders/dsi/pd.py
@@ -47,7 +47,7 @@ class Decoder(srd.Decoder):
     )
     annotation_rows = (
         ('bits', 'Bits', (0,)),
-        ('raw', 'Raw data', (3,)),
+        ('raw-vals', 'Raw data', (3,)),
         ('fields', 'Fields', (1, 2)),
     )
 
@@ -56,7 +56,6 @@ class Decoder(srd.Decoder):
 
     def reset(self):
         self.samplerate = None
-        self.samplenum = None
         self.edges, self.bits, self.ss_es_bits = [], [], []
         self.state = 'IDLE'
 
@@ -112,6 +111,7 @@ class Decoder(srd.Decoder):
             raise SamplerateError('Cannot decode without samplerate.')
         bit = 0
         while True:
+            # TODO: Come up with more appropriate self.wait() conditions.
             (self.dsi,) = self.wait()
             if self.options['polarity'] == 'active-high':
                 self.dsi ^= 1 # Invert.
