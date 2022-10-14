1379117213f2cf052d2343562023082b9f0ff528
dreamsourcelabTAI <tzz@dreamsourcelab.com>
2022-10-14 09:58:01 +0800

    A new multilingual solution

---
+++ DSView/pv/ui/string_ids.h
@@ -0,0 +1,39 @@
+/*
+ * This file is part of the DSView project.
+ * DSView is based on PulseView.
+ *
+ * Copyright (C) 2022 DreamSourceLab <support@dreamsourcelab.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#ifndef STRING_IDS_H
+#define STRING_IDS_H
+
+#define     STR_PAGE_MAIN       1
+#define     STR_PAGE_MSG        2
+#define     STR_PAGE_DEVICE     3
+#define     STR_PAGE_TOOLBAR    4
+
+#define IDS_TOOLBAR_MODE
+#define IDS_TOOLBAR_START
+#define IDS_TOOLBAR_STOP
+#define IDS_TOOLBAR_SINGLE
+#define IDS_TOOLBAR_INSTANT
+#define IDS_TOOLBAR_SINGLE_ACTION
+#define IDS_TOOLBAR_REPEAT_ACTION
+#define IDS_TOOLBAR_OPTIONS
+
+#endif
