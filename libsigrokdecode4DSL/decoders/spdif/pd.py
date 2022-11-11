bdcc3695ed8326875554ce7f0a9b91cd34e053aa
yunyaobaihong <896458252@qq.com>
2022-11-11 18:00:05 +0800

    spdif

--- libsigrokdecode4DSL/decoders/spdif/pd.py
+++ libsigrokdecode4DSL/decoders/spdif/pd.py
@@ -2,6 +2,7 @@
 ## This file is part of the libsigrokdecode project.
 ##
 ## Copyright (C) 2014 Guenther Wenninger <robin@bitschubbser.org>
+## Copyright (C) 2022 DreamSourceLab <support@dreamsourcelab.com>
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
