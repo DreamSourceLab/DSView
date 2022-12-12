7a2c8fd7bc9920278aad14179577d976468dbf3d
dreamsourcelabTAI <tzz@dreamsourcelab.com>
2022-12-12 11:11:49 +0800

    version config file

--- DSView/config.h
+++ DSView/config.h
@@ -29,7 +29,7 @@
 /* DSView version information */
 #define DS_VERSION_MAJOR 1
 #define DS_VERSION_MINOR 2
-#define DS_VERSION_MICRO 1
-#define DS_VERSION_STRING "1.2.1"
+#define DS_VERSION_MICRO 2
+#define DS_VERSION_STRING "1.2.2"
 
 #endif
