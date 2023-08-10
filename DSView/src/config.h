3866db3522b1c0830d6c2c0340095907d63bb28f
dreamsourcelabTAI <tzz@dreamsourcelab.com>
2023-08-10 08:52:45 +0800

    fix: Unable to compile with cmake

---
+++ DSView/src/config.h
@@ -0,0 +1,35 @@
+/*
+ * This file is part of the PulseView project.
+ * DSView is based on PulseView.
+ * 
+ * Copyright (C) 2012 Alexandru Gagniuc <mr.nuke.me@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _DSVIEW_CONFIG_H
+#define _DSVIEW_CONFIG_H
+
+/* Application details */
+#define DS_TITLE "DSView"
+#define DS_DESCRIPTION "A GUI for instruments of DreamSourceLab"
+#define DS_BIN_NAME "DSView"
+
+/* DSView version information */
+#define DS_VERSION_MAJOR 1
+#define DS_VERSION_MINOR 3
+#define DS_VERSION_MICRO 0
+#define DS_VERSION_STRING "1.3.0"
+
+#endif
