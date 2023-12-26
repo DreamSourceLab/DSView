449aed5831ef92c7ead4790d019dbcb76d81bbc9
dreamsourcelabTAI <tzz@dreamsourcelab.com>
2023-12-26 17:42:23 +0800

    update version to v1.3.2-dev1

--- DSView/config.h
+++ DSView/config.h
@@ -29,7 +29,7 @@
 /* DSView version information */
 #define DS_VERSION_MAJOR 1
 #define DS_VERSION_MINOR 3
-#define DS_VERSION_MICRO 1
-#define DS_VERSION_STRING "1.3.1"
+#define DS_VERSION_MICRO 2
+#define DS_VERSION_STRING "1.3.2-dev1"
 
 #endif
